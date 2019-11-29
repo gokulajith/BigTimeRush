814032b7b885afe67a17c6ae1ed20e9b78e535d5
gokul_ajith@brown.edu <gokulajith@Gokuls-MacBook-Pro.local>
2019-11-29 15:16:02 -0500

    Graph Edges Added

--- preprocess.py
+++ preprocess.py
@@ -75,18 +75,6 @@ def get_plays(data):
 
 def get_data(file_name):
 	"""
-	Loads the NCI dataset from an sdf file.
-
-	After getting back a list of all the molecules in the .sdf file,
-	there's a little more preprocessing to do. First, you need to one hot
-	encode the nodes of the molecule to be a 2d numpy array of shape
-	(num_atoms, 119) of type np.float32 (see play.py for more details).
-	After the nodes field has been taken care of, shuffle the list of
-	molecules, and return a train/test split of 0.9/0.1.
-
-	:param file_name: string, name of data file
-	:return: train_data, test_data. Two lists of shuffled molecules that have had their
-	nodes turned into a 2d numpy matrix, and of split 0.9 to 0.1.
 	"""
 	np.set_printoptions(threshold=sys.maxsize)
 	test_fraction = 0.1
